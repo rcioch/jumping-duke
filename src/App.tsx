a2e99a8cc7cf12700c44487b9d14bfc34d46b655
Robert Cioch <robert.cioch@outlook.com>
2024-06-03 15:19:59 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import Game from '@/game/components/Game';
 
 function App() {
   return (
-    <>
-      <p className='read-the-docs'>JumpingDuke - 25 Anniversary Edition</p>
+    <div className='column'>
+      <p className='title'>JumpingDuke - 25 Anniversary Edition</p>
+      <a className='link' href='https://github.com/rcioch/jumping-duke'>
+        https://github.com/rcioch/jumping-duke
+      </a>
       <Game />
-    </>
+    </div>
   );
 }
 
--- src/game/components/GameContent.tsx
+++ src/game/components/GameContent.tsx
@@ -47,10 +47,10 @@ function GameContent() {
   if (engine) {
     return (
       <>
-        <Background engine={engine} key={engine.level} />
+        <Background engine={engine} key={`bg-${engine.level}`} />
         <Floors engine={engine} key={counter} />
         <Duke engine={engine} />
-        <Stats engine={engine} key={engine.level} />
+        <Stats engine={engine} key={`stats-${engine.level}`} />
       </>
     );
   }
